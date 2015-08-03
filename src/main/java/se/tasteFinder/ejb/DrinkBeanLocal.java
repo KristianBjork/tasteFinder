afbde25d61ac484b461aa5f5d299f61f37670f7a
U-STRALFORS\bjokri <bjokri@PCSE66WPSY1.stralfors.se>
2015-08-03 14:10:33 +0200

    Update

--- src/main/java/se/tasteFinder/ejb/DrinkBeanLocal.java
+++
@@ -1,8 +0,0 @@
-package se.tasteFinder.ejb;
-
-import javax.ejb.Local;
-
-@Local
-public interface DrinkBeanLocal {
-
-}
--- src/main/java/se/tasteFinder/ejb/TasteBean.java
+++
@@ -1,5 +0,0 @@
-package se.tasteFinder.ejb;
-
-public class TasteBean {
-
-}
